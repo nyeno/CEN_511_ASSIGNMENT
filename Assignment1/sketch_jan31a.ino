b51eac57eda7d9e57ba89634b3a48def023d564d
Eno Nyenooke Laura <nyenooke.eno@stu.cu.edu.ng>
2021-01-31 15:12:26 +0100

    Add files via upload

---
+++ Assignment1/sketch_jan31a.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("My name is Eno Nyenooke and my matric number is 16cj020720");
+  delay(3000);
+}
